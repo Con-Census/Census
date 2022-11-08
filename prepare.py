f0bca20aff6efa82a8eb61ae15be21b415e7e33a
Everett Clark <everetttylerclark@gmail.com>
2022-11-08 16:17:05 -0600

    new prep function

--- prepare.py
+++ prepare.py
@@ -25,17 +25,14 @@ def get_fema_data():
     z = zipfile.ZipFile(io.BytesIO(response.content))
 
     # read the desired .csv file and drop secondary index column
-    df = pd.read_csv(z.extract('NRI_Table_Counties.csv')).drop(columns='OID_')
-    
-    # lowercase all columns
-    df.columns = df.columns.str.lower()
+    df = pd.read_csv(z.extract('NRI_Table_Counties.csv'))
     
     return df
 
 def prepare_fema(df):
     # Drop columns for additional ID columns or version info
     df.drop(columns=['nri_id', 'statefips', 'countytype', 'countyfips',
-                     'nri_ver', 'stcofips'], inplace=True)
+                     'nri_ver', 'stcofips', 'oid_'], inplace=True)
     
     '''We chose the 3 most costly disasters and removed the others. 
     We combined lighnting and strong wind to represent a severe storm'''
@@ -165,42 +162,19 @@ def prepare_fema(df):
     sr['revenue_per_person'] = sr['revenue_per_person'].str.replace(',','')
     sr['revenue_per_person'] = sr['revenue_per_person'].astype(int)
     
-    # sort states and reset index
-    sr = sr.sort_values(by='stateabbrv').reset_index(drop=True)
-    
     # Create column to merge on
     sr['stateabbrv'] = states
     sr.drop(columns='state', inplace=True)
+    
+    # sort states and reset index
+    sr = sr.sort_values(by='stateabbrv').reset_index(drop=True)
     
     # Merge dataframes
     df = df.merge(sr, how='right', on='stateabbrv')
     
     # Create columns that captures the estimated tax money each county makes based on population
     df['state_funding'] = df.population*df.revenue_per_person
-    
-    # Rearrange columns
-    df = df[['state','stateabbrv', 'county', 'population', 'revenue_per_person', 'state_funding','funding','buildvalue',
-         'agrivalue', 'area','risk_score', 'resl_score', 'resl_value','drought_freq', 'drought_score', 'drought_loss', 
-         'hurricane_freq','hurricane_score', 'hurricane_loss', 'storm_freq', 'storm_score', 'storm_loss']]
-    
-    # Fill NA's as most are areas where a disaster will not strike
-    df=df.fillna(0)
-    
-    df=df.rename(columns={'state':'full_state','stateabbrv':'state'})
-    df.replace('D.C.','DC',inplace=True)
-    
-    # Create population density
-    df['pop_density'] = df.population / df.area
-    
-    # Cost of multiple disasters
-    df['cost']=(df.drought_loss + df.hurricane_loss + df.storm_loss)
-    
-    # How much funding is left over in case of multiple disasters
-    df['support_value'] = (df.state_funding + df.funding) - (df.drought_loss + df.hurricane_loss + df.storm_loss)
-    
-    # Engineer a category for each county on where it falls in reference to "preparedness"
-    df['support_level'] = pd.cut(df.support_value, 4, labels = ['bottom tier', 'below average', 'above average', 'top tier'])
-    
+       
     # State disaster fund data (2018)
     efunds = {'stateabbrv':['AL', 'AK','AZ','AR','CA','CO','CT','DE','D.C.','FL','GA', 'HI','ID','IL','IN','IA','KS','KY','LA',
                             'ME','MD', 'MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK', 'OR',
@@ -214,12 +188,199 @@ def prepare_fema(df):
     efunds = pd.DataFrame(efunds)
     
     # Merge
-    df.merge(efunds, on='stateabbrv', how='right')
+    df = df.merge(efunds, on='stateabbrv', how='right')
+    
+    # Rearrange columns
+    df = df[['state','stateabbrv', 'county', 'population', 'revenue_per_person', 'amount', 'buildvalue',
+         'agrivalue', 'area','risk_score', 'resl_score', 'resl_value','drought_freq', 'drought_score', 'drought_loss', 
+         'hurricane_freq','hurricane_score', 'hurricane_loss', 'storm_freq', 'storm_score', 'storm_loss']]#'funding',
+    
+    # Fill NA's as most are areas where certain disasters will not strike
+    df=df.fillna(0)
+    
+    df=df.rename(columns={'state':'full_state','stateabbrv':'state'})
+    df.replace('D.C.','DC',inplace=True)
+    
+    # Create population density
+    df['pop_density'] = df.population / df.area
+    
+    # Cost of multiple disasters
+    df['cost']=(df.drought_loss + df.hurricane_loss + df.storm_loss)
+    
+    # How much funding is left over in case of multiple disasters
+    df['support_value'] = (df.amount) - (df.drought_loss + df.hurricane_loss + df.storm_loss) # + df.funding
+    
+    # Engineer a category for each county on where it falls in reference to "preparedness"
+    df['support_level'] = pd.cut(df.support_value, 4, labels = ['bottom tier', 'below average', 'above average', 'top tier'])
     
     return df
     
     
+def prep_fema(df):
+    # Drop columns for additional ID columns or version info
+    df.columns = df.columns.str.lower()
+    df.drop(columns=['nri_id', 'statefips', 'countytype', 'countyfips', 'buildvalue', 'agrivalue',
+                     'nri_ver', 'oid_'], inplace=True)
     
+    # Social Vulnerability
+    df = df[df.columns.drop(list(df.filter(regex='sovi')))]
+    # Communitey Resilience
+    df = df[df.columns.drop(list(df.filter(regex='resl')))]
+    # Remove string ratings for each factor
+    df = df[df.columns.drop(list(df.filter(regex='ratng')))]
+    # _EXPB, Exposure - Building Value
+    df = df[df.columns.drop(list(df.filter(regex='expb')))]
+    # Exposure - Agriculture Value
+    df = df[df.columns.drop(list(df.filter(regex='expa')))]
+    # Exposure - Total
+    df = df[df.columns.drop(list(df.filter(regex='expt')))]
+    # _EXPP, Exposure - Population
+    df = df[df.columns.drop(list(df.filter(regex='expp')))]
+    # _EXPPE, Exposure - Population Equivalence
+    df = df[df.columns.drop(list(df.filter(regex='exppe')))]
+    # _HLRB, Historic Loss Ratio - Buildings
+    df = df[df.columns.drop(list(df.filter(regex='hlrb')))]
+    # _HLRP, Historic Loss Ratio - Population
+    df = df[df.columns.drop(list(df.filter(regex='hlrp')))]
+    # _HLRA, Historic Loss Ratio - Agriculture
+    df = df[df.columns.drop(list(df.filter(regex='hlra')))]
+    # _HLRR, Historic Loss Ratio - Total Rating
+    df = df[df.columns.drop(list(df.filter(regex='hlrr')))]
+    # _EALB, Expected Annual Loss - Building Value
+    df = df[df.columns.drop(list(df.filter(regex='ealb')))]
+    # _EALP, Expected Annual Loss - Population
+    df = df[df.columns.drop(list(df.filter(regex='ealp')))]
+    # _EALPE, Expected Annual Loss - Population Equivalence
+    df = df[df.columns.drop(list(df.filter(regex='ealpe')))]
+    # _EALA, Expected Annual Loss - Agriculture Value
+    df = df[df.columns.drop(list(df.filter(regex='eala')))]
+    # Expected Annual Loss Score
+    df = df[df.columns.drop(list(df.filter(regex='eals')))]
+    # _EALR, Expected Annual Loss Rating
+    df = df[df.columns.drop(list(df.filter(regex='ealr')))]
+    # _RISKR, Hazard Type Risk Index Rating
+    df = df[df.columns.drop(list(df.filter(regex='riskr')))]
+    # _NPCTL, National Risk Index - National Percentile - Composite
+    df = df[df.columns.drop(list(df.filter(regex='npctl')))]
+    # _SPCTL, National Risk Index - State Percentile - Composite
+    df = df[df.columns.drop(list(df.filter(regex='spctl')))]
+    # Expected Annual Loss
+    df = df[df.columns.drop(list(df.filter(regex='eal_')))]
+    # Events are recorded over varyiong periods, so we'll only keep frequency
+    df = df[df.columns.drop(list(df.filter(regex='evnts')))]
+    
+    # Sort alphabetically by state
+    df = df.sort_values(by=['state','county'])
+    
+    # Get State funding data
+    state_funding = pd.read_csv('state_funding_2022.csv', index_col=0)
+    
+    # Drop territories that we don't have data or in the FEMA data
+    state_funding = state_funding.iloc[:51,:]
+    
+    states = ['AL','AK','AZ','AR','CA','CO','CT','DE','D.C.','FL','GA','HI','ID','IL','IN','IA','KS',
+              'KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC',
+              'ND','OH','OK','OR','PA','RI','SC', 'SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+    
+    # Create a columm that'll help with joining the two dataframes
+    state_funding['stateabbrv'] = states
+    
+    # Drop $ and , to convert column type
+    state_funding.funding = state_funding.funding.str.replace('$','', regex=True).replace(',','', regex=True)
+    
+    # Make the funding an integer type
+    state_funding.funding = state_funding.funding.astype(int)
+    
+    # Join dataframes
+    df = df.merge(state_funding[['stateabbrv','funding']], how='right', on='stateabbrv')
+    
+    # State Revenue per person, narrow df down to states, dropping regions, and changing column type to integer
+    sr = pd.read_csv('state_revenue.csv')
+    sr = sr.iloc[4:75]
+    sr.dropna(inplace=True)
+    sr = sr[['25-Aug-22','Unnamed: 23']]
+    sr = sr.rename(columns={'Unnamed: 23': 'revenue_per_person', '25-Aug-22':'state'}).iloc[1:]
+    sr = sr.iloc[2:].drop(index=[16,24,31,40,54,60,67])
+    
+    # Create new column
+    sr['revenue_per_person'] = sr['revenue_per_person'].str.replace(',','')
+    sr['revenue_per_person'] = sr['revenue_per_person'].astype(int)
+    
+    # Create column to merge on
+    sr['stateabbrv'] = states
+    sr.drop(columns='state', inplace=True)
+    
+    # sort states and reset index
+    sr = sr.sort_values(by='stateabbrv').reset_index(drop=True)
+    
+    # Merge dataframes
+    df = df.merge(sr, how='right', on='stateabbrv')
+    
+    # Create columns that captures the estimated tax money each county makes based on population
+    df['state_funding'] = df.population*df.revenue_per_person
+       
+    # State disaster fund data (2018)
+    efunds = {'stateabbrv':['AL', 'AK','AZ','AR','CA','CO','CT','DE','D.C.','FL','GA', 'HI','ID','IL','IN','IA','KS','KY','LA',
+                            'ME','MD', 'MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK', 'OR',
+                            'PA', 'RI','SC', 'SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'],
+              'amount':[(56700 + 5287908 + 420000 + 500000),2000000, 8000000, 17569984, 1000000, 12500000, 0, 0, 0, 15284704,
+                        11062041, 0, 0, 0, (114456+119004+485000), 0, 1315138, 0, 1100000, 0, 0, 0, 0, 10000000, 20000000, 0,
+                        0,250000, 2000000, 0, 0, 0, 200000000, 22300000, 12292597, 7500000, 0, 0, 0, 250000, 0, 0, 4000000, 
+                        100000000, (11113142+104100+7093015), 2000000, 0, 77483000, 0, 711200, 500000]}
+    
+    # Create a df
+    efunds = pd.DataFrame(efunds)
+    
+    # Merge
+    df = df.merge(efunds, on='stateabbrv', how='right')
+    
+    # Get total state population
+    statepop = pd.DataFrame(df.groupby('state').population.sum())
+    statepop.rename(columns={'population':'statepop'}, inplace=True)
+    
+    # Merge
+    df = df.merge(statepop, on='state', how='right')
+    
+    # Disaster that would cost the most
+    df['max_cost'] = df[['drgt_ealt', 'hrcn_ealt', 'avln_ealt', 'cwav_ealt', 'erqk_ealt', 'hail_ealt', 
+     'rfld_ealt','lnds_ealt','ltng_ealt','trnd_ealt','hwav_ealt','swnd_ealt',
+     'tsun_ealt','vlcn_ealt','wfir_ealt','wntw_ealt']].max(axis=1)
+    
+    # How much a county would get when disaster strikes
+    df['county_funding'] = df.amount/df.statepop*df.population
+    
+    # Define if there is a deficit between the funding they receive and the cost of disaster
+    df['deficit'] = (df.county_funding) - df.max_cost
+    
+    # Fill NA's as most are areas where certain disasters will not strike
+    df=df.fillna(0)
+    
+    df=df.rename(columns={'state':'full_state','stateabbrv':'state'})
+    df.replace('D.C.','DC',inplace=True)
+    
+    # Create population density
+    df['pop_density'] = df.population / df.area
+    
+    
+    # Engineer a category for each county on where it falls in reference to "preparedness"
+    df['support_level'] = pd.cut(df.deficit, 4, labels = ['bottom tier', 'below average', 'above average', 'top tier'])
+    
+    df = df[['full_state', 'state', 'county', 'population','statepop', 'area', 'pop_density','funding',
+       'revenue_per_person', 'state_funding', 'amount', 'risk_score',
+       'avln_afreq', 'avln_ealt', 'avln_risks', 'cfld_afreq', 'cfld_ealt',
+       'cfld_risks', 'cwav_afreq', 'cwav_ealt', 'cwav_risks', 'drgt_afreq',
+       'drgt_ealt', 'drgt_risks', 'erqk_afreq', 'erqk_ealt', 'erqk_risks',
+       'hail_afreq', 'hail_ealt', 'hail_risks', 'hwav_afreq', 'hwav_ealt',
+       'hwav_risks', 'hrcn_afreq', 'hrcn_ealt', 'hrcn_risks', 'istm_afreq',
+       'istm_ealt', 'istm_risks', 'lnds_afreq', 'lnds_ealt', 'lnds_risks',
+       'ltng_afreq', 'ltng_ealt', 'ltng_risks', 'rfld_afreq', 'rfld_ealt',
+       'rfld_risks', 'swnd_afreq', 'swnd_ealt', 'swnd_risks', 'trnd_afreq',
+       'trnd_ealt', 'trnd_risks', 'tsun_afreq', 'tsun_ealt', 'tsun_risks',
+       'vlcn_afreq', 'vlcn_ealt', 'vlcn_risks', 'wfir_afreq', 'wfir_ealt',
+       'wfir_risks', 'wntw_afreq', 'wntw_ealt', 'wntw_risks', 'max_cost',
+       'county_funding', 'deficit', 'support_level']]
+    
+    return df    
     
     
     
